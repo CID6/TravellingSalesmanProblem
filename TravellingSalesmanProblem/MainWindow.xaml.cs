b880c099b321f8345ddeebc38d6f19c3bf989647
Cezary G <ttgovin@gmail.com>
2020-03-23 22:43:51 +0100

    Dodaj pliki projektów.

---
+++ TravellingSalesmanProblem/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TravellingSalesmanProblem
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Main1();
+        }
+
+
+        public void Main1()
+        {
+            //problem jest instancją N miast o losowych koordynatach. "a problem instance π"
+            //ilosc węzłów jest parametrem. Wartości krawędzi są losowe. Każde miasto ma swoje własne, losowe koordynaty. Wartości krawędzi to fizyczny dystans
+            //między nimi.
+            Problem problem = new Problem(7);      
+            //Solution jest losowym rozwiazaniem. Porządek odwiedzonych miast jest całkowicie losowy niezależnie od temperatury. Nie daje to zbyt dobrych rezultatów,
+            //ale jest proste w implementacji.
+            Solution initialSolution = new Solution(problem);   //"Initial Solution s 0"
+            Solution bestSolution = initialSolution;
+            Solution newSolution;
+
+            //"Initial Temperature" temperatura układu. Zwiększenie jej zwiększa ilość iteracji, ale też zwiększa szansę na to, że w Metropolis criterion 
+            //zostanie wybrane gorsze rozwiązanie.
+            double temperature = 20;
+            //Cooling Scheme. W tym przypadku wykladniczy. Im większa dokładność do 1, tym więcej iteracji.
+            double coolingRate = 0.999;
+            //iteration count
+            int iterations = 0;
+            //dobór początkowych parametrów temperature, cooling rate i stopping criterion jest w tym przypadku wyłącznie intuicyjny, bazowałem na
+            //ilości iteracji i wartości Score.
+            while(temperature > 0.0001) //Stopping Criterion. W tym przypadku, stała wartość temperatury.
+            {
+
+                //Exploration Criterion. W tym przypadku, całkowicie losowa nowa ścieżka.
+                //W przypadku takiego Exploration Criterion, annealing nie jest zbyt wydajny. Wkrótcę wyślę nową, poprawioną wersję programu.
+                //Na razie jednak, ze względu na zbliżający się deadline taki musi wystarczyć.
+                //Poprawny Exploration Criterion, powinien bazować na zmianach w ścieżce w zależności od temperatury.
+                //I mniejsza temperatura, zmiany powinny być mniejsze. Wysoka temperatura powinna praktycznie losować na nowo całe rozwiązanie.
+                newSolution = new Solution(problem); 
+
+                iterations++;
+
+                //delta(s, s')
+                double delta = newSolution.Score - bestSolution.Score;
+
+                //Acceptance Criterion. W tym przypadku, Metropolis-based criteria. Na zajęciach było wspomniane, że takiego musimy użyć.
+                if(delta <= 0 || (delta > 0 && Math.Exp(-delta/temperature) > problem.RNG.NextDouble()))
+                {
+                    bestSolution = newSolution;
+
+                    //debug output. Dla każdego nowego bestSolution, wypisuję jego dane.
+                    outputTB.Text += "Path: ";
+                    foreach(int j in bestSolution.CitySequence)
+                    {
+                        outputTB.Text += j + " ";
+                    }
+                    outputTB.Text += "    Temperature: " + temperature + "    Score: " + bestSolution.Score + "    Iteration: " + iterations + "\n";
+                }
+                //Cooling Scheme. 
+                temperature = temperature * coolingRate;
+
+            }
+        }
+    }
+}
---
+++ TravellingSalesmanProblem/Module.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmanProblem
+{
+    class City
+    {
+        public int ID { get; }
+        public double X { get; }
+        public double Y { get; }
+
+        public City(double _X, double _Y, int _ID)
+        {
+            X = _X;
+            Y = _Y;
+            ID = _ID;
+        }
+
+        public static double Distance(City _pierwsze, City _drugie)
+        {
+            return Math.Sqrt((_drugie.X - _pierwsze.X) * (_drugie.X - _pierwsze.X) + (_drugie.Y - _pierwsze.Y) * (_drugie.Y - _pierwsze.Y));
+        }
+    }
+
+    class Problem
+    {
+        public City[] Cities { get; }
+        public Random RNG { get; }
+
+        public Problem(int _cityCount)
+        {
+            Cities = new City[_cityCount];
+            RNG = new Random();
+            for (int i = 0; i < _cityCount; i++)
+            {
+                Cities[i] = new City(RNG.NextDouble() * 100, RNG.NextDouble() * 100, i);
+            }
+        }
+    }
+
+    class Solution
+    {
+        Problem problem;
+        public int[] CitySequence { get; internal set; }
+        public double Score { get; internal set;  }
+
+        public Solution(Problem _problem)
+        {
+            problem = _problem;
+            CitySequence = new int[problem.Cities.Length];
+            GenerateRandomSolution();
+            CalculateScore();
+        }
+
+        private void GenerateRandomSolution()
+        {
+            int max = CitySequence.Length;
+            CitySequence = Enumerable.Range(0, max).OrderBy(c => problem.RNG.Next()).ToArray();
+            int value = CitySequence[0];
+            for (int i = 1; i < CitySequence.Length; i++)
+            {
+                if(CitySequence[i] == 0)
+                {
+                    CitySequence[0] = 0;
+                    CitySequence[i] = value;
+                }
+            }
+        }
+
+        private void CalculateScore()
+        {
+            double suma = 0;
+            for (int i = 0; i < CitySequence.Length - 1; i++)
+            {
+                suma += City.Distance(problem.Cities[CitySequence[i]], problem.Cities[CitySequence[i + 1]]);
+            }
+            suma += City.Distance(problem.Cities[CitySequence[CitySequence.Length - 1]], problem.Cities[CitySequence[0]]);
+            Score = suma;
+        }
+
+    }
+
+}
